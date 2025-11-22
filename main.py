9f3c277ddde47d71c8d1659dbaf3833048caf48d
HRagab1 <hazim.t.ragab@gmail.com>
2025-11-21 21:28:37 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import importlib.util
+import sys
+import os
+import socket
+import time
+from datetime import datetime
+import starlink_grpc
+from grpc import RpcError
+
+def run(): #Main method
+
+if __name__ == '__main__':
+    run()
