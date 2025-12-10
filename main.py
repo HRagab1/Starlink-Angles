549f5d2981e2638a8b5aa236c6d8765721eb306b
HRagab1 <hazim.t.ragab@gmail.com>
2025-12-10 13:10:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import time
 import starlink_grpc
 from grpc import RpcError
+from datetime import datetime
 
 def print_stats():
     try: 
         status_data = starlink_grpc.status_data('192.168.100.1')
         state = status_data.get('state', {})
+        info = status_data.get('device_info', {})
         
-        print(f"Test Results:")
-        print(f"DEVICE ID:             {state.get('id', 'N/A')}")
-        print(f"HARDWARE:              {state.get('hardware_version', 'N/A')}")
-        print(f"SOFTWARE:              {state.get('software_version', 'N/A')}")
-        print(f"LATENCY:               {state.get('pop_ping_latency_ms', 0.0):.2f} ms")
+        print("Test Results:")
+        print(f"TIME:                  {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+        print(f"DEVICE ID:             {info.get('id', 'N/A')}")
+        print(f"HARDWARE:              {info.get('hardware_version', 'N/A')}")
+        print(f"SOFTWARE:              {info.get('software_version', 'N/A')}")
         print(f"DROP RATE:             {state.get('pop_ping_drop_rate', 0.0) * 100:.2f} %")
         print(f"DOWNLINK THROUGHPUT:   {state.get('downlink_throughput_bps', 0.0) / 1e6:.2f} Mbps")
         print(f"UPLINK THROUGHPUT:     {state.get('uplink_throughput_bps', 0.0) / 1e6:.2f} Mbps\n------------------------------")
+        print(f"LATENCY:               {state.get('pop_ping_latency_ms', 0.0):.2f} ms")
         print(f"USER DIRECTION:        {state.get('direction_azimuth', 0.0):.2f}")   #NEEDS GPS TO BE ENABLED TO RUN WITHOUT ERRORS
         print(f"USER ANGLE:            {state.get('direction_elevation', 0.0):.2f}") #NEEDS GPS TO BE ENABLED TO RUN WITHOUT ERRORS
         print(f"NUMBER OF SATELLITES:  {state.get('gps_sats', 0.0):.2f}\n------------------------------")
