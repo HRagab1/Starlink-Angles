8987ba3a4d76e34cfe1486909b12eed0b596f95b
HRagab1 <hazim.t.ragab@gmail.com>
2025-12-10 13:49:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import time
 import starlink_grpc
 from grpc import RpcError
 from datetime import datetime
+import sys
 
-def print_stats(state, info):
-    try:        
+#Prints Status
+def print_stats():
+    try:   
+        #Update Info
+        status_data = starlink_grpc.status_data('192.168.100.1')
+        state = status_data.get('state', {})
+        info = status_data.get('device_info', {})
+
+        #Prints Info
         print("Test Results:")
         print(f"TIME:                  {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
         print(f"DEVICE ID:             {info.get('id', 'N/A')}")
@@ -14,8 +22,8 @@ def print_stats(state, info):
         print(f"DOWNLINK THROUGHPUT:   {state.get('downlink_throughput_bps', 0.0) / 1e6:.2f} Mbps")
         print(f"UPLINK THROUGHPUT:     {state.get('uplink_throughput_bps', 0.0) / 1e6:.2f} Mbps\n------------------------------")
         print(f"LATENCY:               {state.get('pop_ping_latency_ms', 0.0):.2f} ms")
-        print(f"USER DIRECTION:        {state.get('direction_azimuth', 0.0):.2f}")   #NEEDS GPS TO BE ENABLED TO RUN WITHOUT ERRORS
-        print(f"USER ANGLE:            {state.get('direction_elevation', 0.0):.2f}") #NEEDS GPS TO BE ENABLED TO RUN WITHOUT ERRORS
+        print(f"USER DIRECTION:        {state.get('direction_azimuth', 0.0):.2f}")
+        print(f"USER ANGLE:            {state.get('direction_elevation', 0.0):.2f}") 
         print(f"NUMBER OF SATELLITES:  {state.get('gps_sats', 0.0):.2f}\n------------------------------")
     except RpcError as e:
         print(f"COMMUNICATION ERROR: {e}")
@@ -23,6 +31,18 @@ def print_stats(state, info):
         print(f"UNEXPECTED ERROR: {e}")
 
 if __name__ == '__main__':
+    #Checks GPS Status
+    try: 
+        check = starlink_grpc.status_data('192.168.100.1')
+        checker = check.get('state', {})
+        if not checker.get('gps_enabled', False):
+            print("ERROR: GPS NOT ENABLED")
+            sys.exit(0)
+    except Exception as e:
+        print(f"UNEXPECTED ERROR: {e}")
+        sys.exit(0)
+
+    #User's Update Interval
     while True:
         try:
             sec_update = int(input("Update experiment every __ second(s): "))
@@ -31,17 +51,11 @@ if __name__ == '__main__':
             break
         except ValueError as e:
             print(f"Invalid input! {e}")
-    
+
+    #Main Loop
     try: 
         while True:
-            status_data = starlink_grpc.status_data('192.168.100.1')
-            state = status_data.get('state', {})
-            info = status_data.get('device_info', {})
-            if state.get('gps_enabled', False):
-                print_stats(state, info)
-                time.sleep(sec_update)
-            else:
-                print("ERROR: GPS NOT ENABLED")
-                break
+            print_stats()
+            time.sleep(sec_update)
     except KeyboardInterrupt:
         print("-------------------\nExperiment ended\n-------------------")
